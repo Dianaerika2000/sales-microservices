0852415dbcd048f4ab68dcb830b888b42378183a
dianaM <diana.montano@assuresoftbolivia.com>
2023-11-24 02:54:34 -0400

    Add connection to data base

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,14 +3,14 @@ import { ConfigService } from "@nestjs/config";
 const configService = new ConfigService();
 
 module.exports = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password : 'postgres',
-  database: 'db_sales',
-  //url: configService.get('DB_URL'),
-  //ssl: true,
+  // type: 'postgres',
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'postgres',
+  // password : 'postgres',
+  // database: 'db_sales',
+  url: configService.get('DB_URL'),
+  ssl: true,
   entities: ['src/**/**/*.entity{.ts,.js}'],
   seeds: ['src/database/seeds/**/*{.ts,.js}'],
   factories: ['src/database/factories/**/*{.ts,.js}'],
